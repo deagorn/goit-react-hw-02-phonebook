e4e164f85d8b08095af52298f7339936cf4bbfb2
Nadiia <emmideagorn@gmail.com>
2024-02-21 14:01:26 +0200

    phoneBook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,77 @@
 import { nanoid } from "nanoid";
 import { Component } from "react";
 import ContactList from "./ContactList/ContactList";
+import  ContactForm  from "./ContactForm/ContactForm";
+import Filter from "./Filter/Filter";
+import s from './App.module.css'
+
 
 export class App extends Component {
 
-  state = {
-  contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+ state = {
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
-  name: ''
-  }
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: '',
+}
 
   handleChanheName = e => {
     this.setState({ name: e.target.value })
   }
   
-  handleAddUser = () => {
-    const newUser = { id: nanoid(), name: this.state.name }
-    this.setState = (prev => ({contacts: [...prev.contacts, newUser], name: ''}))
-  }
-  
-  getVisibleContacts = () => {
+  handleAddContact = (name, number) => {
     const { contacts } = this.state;
-    return contacts; 
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    }
+
+     if (contacts.some((contact) => contact.name === name)) {
+      alert(`${name} is already in contacts. Try again! `);
+      return;
+    }
+
+    this.setState((prev) => ({
+      contacts: [...prev.contacts, newContact],
+    }))
+
+  }
+
+  deleteContact = (contactId) => {
+    this.setState((prev) => ({
+      contacts: prev.contacts.filter((contact) => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = (e) => {
+    this.setState({ filter: e.target.value });
+  };
+
+   getVisibleContacts = () => {
+     const { contacts, filter } = this.state;
+     const normalizedFilter = filter.toLowerCase();
+     return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
 
   }
 
   render() {
-    const { name } = this.state;
+    const { filter } = this.state;
     const visible = this.getVisibleContacts();
     return ( 
-    <div>
-        <input type="text" name="name" value={name} onChange={this.handleChanheName } required  />
-        <button onClick={this.handleAddUser}>Add contact</button>
-        <ContactList contacts={visible}/>
+      <div className={s.container}>
+        <h1 className={s.title}>Phonebook</h1>
+        <ContactForm onSubmit={this.handleAddContact} />
+
+  <h1 className={s.title}>Contacts</h1>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList contacts={visible} onDeleteContact={this.deleteContact} />
+        
     </div>
   );}
   
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,17 @@
-const ContactList = ({ contacts }) => {
-    <ul>
-        {contacts.length>0? contacts.map(({ id, name }) => (
-            <li key={id}>{name}</li>
-        )
-        ):<div>rvdrr</div>}
-    </ul>
-}
+import s from "./ContactList.module.css"
 
+const ContactList = ({ contacts, onDeleteContact }) => (
+     <ul className={s.listContainer}>
+        {contacts.length>0? contacts.map(({ id, name, number}) => (
+            <li key={id} className={s.listItem}>  <div className={s.contactInfo}>
+            <span className={s.contactName}> {name} </span> : {number} </div>
+            <button type="button" onClick={() => onDeleteContact(id)} className={s.deleteButton}>
+          Delete
+                </button>
+            </li>
+        )
+        ):<div></div>}
+    </ul>
+
+)
 export default ContactList;
